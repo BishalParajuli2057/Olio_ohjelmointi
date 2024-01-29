48e2d9301dd90d72c81da565dadc4e0c7cd17de1
BishalParajuli2057 <bishalparajuli2057@gmail.com>
2024-01-29 20:04:57 +0200

    Update Zoo.java

--- week_1/src/Zoo.java
+++ week_1/src/Zoo.java
@@ -22,8 +22,8 @@ public class Zoo {
     }
 
     public void runAnimals (int rounds){
-        for (int i=0; i<rounds; i++){
-            for(Animal animal : animals){
+      for(Animal animal : animals){
+        for (int i = 0; i < rounds; i++){
                 animal.run();
             }
         }
