b3a58e86384ba8056aded9c165ef1e4da96d9a52
BishalParajuli2057 <bishalparajuli2057@gmail.com>
2024-01-29 19:12:59 +0200

    2

--- week_1/src/App.java
+++ week_1/src/App.java
@@ -1,42 +1,55 @@
 import java.util.Scanner;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Anna eläintarhalle nimi:");
-        String zooName = scanner.nextLine();
+        String zooName = sc.nextLine();
         Zoo zoo = new Zoo(zooName);
 
-        while (true) {
+        boolean exit = false;
+        while (!exit) {
             System.out.println("1) Luo uusi eläin, 2) Listaa kaikki eläimet, 3) Juoksuta eläimiä, 0) Lopeta ohjelma");
-            int choice = scanner.nextInt();
-            scanner.nextLine(); 
-            
 
-            if (choice == 1) {
-                System.out.println("Mikä laji?");
-                String species = scanner.nextLine();
-                System.out.println("Anna eläimen nimi:");
-                String name = scanner.nextLine();
-                System.out.println("Anna eläimen ikä:");
-                int age = scanner.nextInt();
-                
+            if (sc.hasNext()) {
+                int choice = Integer.parseInt(sc.nextLine());
 
-                Animal animal = new Animal(species, name, age);
-                zoo.addanimal (animal);
-            } else if (choice == 2) {
-                zoo.listAnimals();
-            } else if (choice == 3) {
-                System.out.println("Kuinka monta kierrosta?");
-                int rounds = scanner.nextInt();
-                zoo.runAnimals(rounds);
-                
-            } else if (choice == 0) {
-                System.out.println("Kiitos ohjelman käytöstä.");
-                break;
-            } else {
-                System.out.println("Syöte oli väärä");
+                switch (choice) {
+                    case 1:
+                        System.out.println("Mikä laji?");
+                        String species = sc.nextLine();
+                        System.out.println("Anna eläimen nimi:");
+                        String name = sc.nextLine();
+                        System.out.println("Anna eläimen ikä:");
+                        int age = Integer.parseInt(sc.nextLine());
+
+                        Animal animal = new Animal(species, name, age);
+                        zoo.addanimal(animal);
+                        break;
+
+                    case 2:
+                        zoo.listAnimals();
+                        break;
+
+                    case 3:
+                        System.out.println("Kuinka monta kierrosta?");
+                        int rounds = Integer.parseInt(sc.nextLine());
+                        zoo.runAnimals(rounds);
+                        break;
+
+                    case 0:
+                        System.out.println("Kiitos ohjelman käytöstä.");
+                        exit = true;
+                        break;
+
+                    default:
+                        System.out.println("Syöte oli väärä");
+                        break;
+                }
             }
         }
+        sc.close();
     }
 }
